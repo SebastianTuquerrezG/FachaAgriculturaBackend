dac3dfa77e8d4c9b06f36550ee5c21b2402b47ad
CrisQR <cdqr27@gmail.com>
2024-10-05 17:00:46 -0500

    chore: Refactor geolocation code and update main.py

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ def read_item():
         "df_grid_timeseries": df_grid_timeseries.to_dict(orient="records")
     }
 
-# Tu clave API de Google
 key = os.getenv('GOOGLE_API_KEY')
 url = f'https://www.googleapis.com/geolocation/v1/geolocate?key={key}'
 
@@ -32,15 +31,12 @@ def geolocate():
         "considerIp": "true"
     }
 
-    # Hacer la petición POST
+    # Petición POST
     response = requests.post(url, json=data)
 
-    # Verificar el estado de la respuesta
     if response.status_code == 200:
-        # La solicitud fue exitosa
         respuesta = response.json()
-        
-        # Extraer latitud y longitud
+
         latitud = respuesta.get('location', {}).get('lat')
         longitud = respuesta.get('location', {}).get('lng')
         
@@ -49,5 +45,4 @@ def geolocate():
             "longitud": longitud
         }
     else:
-        # Hubo un error con la solicitud
         return {"error": response.status_code, "message": response.text}
